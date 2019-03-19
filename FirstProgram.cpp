77d5a24dca811b04395adaee0b8e9b548acd3bc2
joss.simon <joss.simon@gmail.com>
2019-03-19 19:28:09 +0100

    New file

--- FirstProgram.cpp
+++ FirstProgram.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+int add(int x, int y);
+
 int getValueFromUser()
 {
     int input{0};
@@ -16,10 +18,17 @@ void printDouble(int x)
     std::cout << x << " doubleed is : " << 2 * x << '\n';
 }
 
+void printSum(int x, int y)
+{
+    std::cout << x << " + " << y << " = " << add(x, y) << '\n';
+}
+
 int main()
 {
     // my first program
     printDouble(getValueFromUser());
+
+    printSum(getValueFromUser(), getValueFromUser());
 
     return 0;
 }
